85ba54b1b5d8e5a1bad9155290c90431116db85d
Aaron Francis <Aarondfrancis@gmail.com>
2023-04-12 08:42:49 -0500

    Put the v back at the beginning

--- build/versions.sh
+++ build/versions.sh
@@ -9,4 +9,4 @@ set -e
 dir="$(dirname "$(realpath "$0")")"
 
 # List all of the available Nova versions and write them to the versions.txt file.
-composer show --all --no-ansi laravel/nova '^4.13' | grep 'versions :' | sed 's/versions : \* //' | grep -oE '4\.[0-9]+\.[0-9]+' > "$dir/versions.txt"
+composer show --all --no-ansi laravel/nova '^4.13' | grep 'versions :' | sed 's/versions : \* //' | grep -oE '4\.[0-9]+\.[0-9]+' | sed 's/^/v/' > "$dir/versions.txt"
